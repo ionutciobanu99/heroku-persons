57aeb5a601ed0dccab48630374e3fb7d5d766e10
AndreeaaBadea <100729259+AndreeaaBadea@users.noreply.github.com>
2022-04-12 13:11:41 +0300

    Add files via upload

---
+++ src/main/java/com/example/liquidbaseprojectAB/LiquidbaseProjectAbApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquidbaseprojectAB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidbaseProjectAbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidbaseProjectAbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/liquidbaseprojectAB/LiquidbaseProjectAbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquidbaseprojectAB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidbaseProjectAbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
